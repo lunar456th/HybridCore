6ad1d338794b81f4ca7be7a237bfcf3b2a816047
lunar456th <lunar456th@naver.com>
2019-01-06 00:17:13 +0900

    .

--- Processor.v
+++ Processor.v
@@ -1,8 +1,34 @@
+module main # (
+	)	(
+
+
 module Processor # (
-    parameter NUM_CORES = 16
+    parameter NUM_CORES = 16,
+	parameter DQ_WIDTH = 16,
+	parameter ECC_TEST = "OFF",
+	parameter nBANK_MACHS = 4,
+	parameter ADDR_WIDTH = 28,
+	parameter nCK_PER_CLK = 4
 	)	(
 	input clk, // 클락
 	input reset, // 리셋
+	
+	// AXI Memory In/Out Ports
+	inout [15:0] ddr3_dq,
+	inout [1:0] ddr3_dqs_n,
+	inout [1:0] ddr3_dqs_p,
+	output [13:0] ddr3_addr,
+	output [2:0] ddr3_ba,
+	output ddr3_ras_n,
+	output ddr3_cas_n,
+	output ddr3_we_n,
+	output ddr3_reset_n,
+	output [0:0] ddr3_ck_p,
+	output [0:0] ddr3_ck_n,
+	output [0:0] ddr3_cke,
+	output [0:0] ddr3_cs_n,
+	output [1:0] ddr3_dm,
+	output [0:0] ddr3_odt,
 
 	// AXI Memory 와 소통하는 in/out
 	output [31:0] device_addr,
@@ -43,7 +69,7 @@ module Processor # (
     assign device_write_en = memory_wren;
     assign memory_read_val = device_data_in;
     assign device_data_out = memory_write_val;
-
+	
 
 	genvar i;
 	generate
@@ -81,8 +107,9 @@ module Processor # (
         end
     end
 
-	// Dynamic Arbitration using arbiter
-	Arbiter #( 
+
+	// Dynamic Arbitration for accessing external memory using arbiter
+	Arbiter # (
 		.NUM_CORES(NUM_CORES)
 	) _Arbiter(
 		.clk(clk),
@@ -90,5 +117,123 @@ module Processor # (
 		.request(core_request),
 		.core_select(core_enable)
 	);
+
+
+	localparam STATE_IDLE = 3'd0;
+	localparam STATE_WRITE = 3'd1;
+	localparam STATE_WRITE_DONE = 3'd2;
+	localparam STATE_READ = 3'd3;
+	localparam STATE_READ_DONE = 3'd4;
+	localparam STATE_PARK = 3'd5;
+
+	reg [127:0] data_to_write = { 32'hcafecafe, 32'hfaceface, 32'hbabebabe, 32'hABCD1234 };
+	reg [127:0] data_read_from_memory = 128'd0;
+	reg [9:0] pwr_on_rst_ctr = 1023;
+
+	localparam DATA_WIDTH = 16;
+	localparam PAYLOAD_WIDTH = (ECC_TEST == "OFF") ? DATA_WIDTH : DQ_WIDTH;
+	localparam APP_DATA_WIDTH = 2 * nCK_PER_CLK * PAYLOAD_WIDTH;
+	localparam APP_MASK_WIDTH = APP_DATA_WIDTH / 8;
+	
+	wire init_calib_complete;
+
+	reg [ADDR_WIDTH-1:0] app_addr = 0;
+	reg [2:0] app_cmd = 0;
+	reg app_en;
+	reg [APP_DATA_WIDTH-1:0] app_wdf_data;
+	wire app_wdf_end = 1;
+	wire [APP_MASK_WIDTH-1:0] app_wdf_mask = 0;
+	reg app_wdf_wren;
+	wire [127:0] app_rd_data;
+	wire app_rd_data_end;
+	wire app_rd_data_valid;
+	wire app_rdy;
+	wire app_wdf_rdy;
+	wire app_sr_active;
+	wire app_ref_ack;
+	wire app_zq_ack;
+	wire ui_clk;
+	wire ui_clk_sync_rst;
+	wire [11:0] device_temp = 0;
+	wire sys_rst = (pwr_on_rst_ctr == 0);
+
+	always @ (posedge clk)
+	begin
+		if (pwr_on_rst_ctr)
+		begin
+			pwr_on_rst_ctr <= pwr_on_rst_ctr - 1 ;
+		end
+	end
+
+`ifdef SKIP_CALIB // skip calibration wires
+	wire calib_tap_req;
+	reg calib_tap_load;
+	reg [6:0] calib_tap_addr;
+	reg [7:0] calib_tap_val;
+	reg calib_tap_load_done;
+`endif
+
+
+	// External Memory Interface
+	ExternalMemory _ExternalMemory
+	(
+		// Memory interface ports
+		.ddr3_dq(ddr3_dq),
+		.ddr3_dqs_n(ddr3_dqs_n),
+		.ddr3_dqs_p(ddr3_dqs_p),
+
+		.ddr3_addr(ddr3_addr),
+		.ddr3_ba(ddr3_ba),
+		.ddr3_ras_n(ddr3_ras_n),
+		.ddr3_cas_n(ddr3_cas_n),
+		.ddr3_we_n(ddr3_we_n),
+		.ddr3_reset_n(ddr3_reset_n),
+		.ddr3_ck_p(ddr3_ck_p),
+		.ddr3_ck_n(ddr3_ck_n),
+		.ddr3_cke(ddr3_cke),
+		.ddr3_cs_n(ddr3_cs_n),
+		.ddr3_dm(ddr3_dm),
+		.ddr3_odt(ddr3_odt),
+
+		// Application interface ports
+		.app_addr(app_addr),
+		.app_cmd(app_cmd),
+		.app_en(app_en),
+		.app_wdf_data(app_wdf_data),
+		.app_wdf_end(app_wdf_end),
+		.app_wdf_mask(app_wdf_mask),
+		.app_wdf_wren(app_wdf_wren),
+		.app_rd_data(app_rd_data),
+		.app_rd_data_end(app_rd_data_end),
+		.app_rd_data_valid(app_rd_data_valid),
+		.app_rdy(app_rdy),
+		.app_wdf_rdy(app_wdf_rdy),
+		.app_sr_req(1'b0),
+		.app_ref_req(1'b0),
+		.app_zq_req(1'b0),
+		.app_sr_active(app_sr_active),
+		.app_ref_ack(app_ref_ack),
+		.app_zq_ack(app_zq_ack),
+		.ui_clk(ui_clk),
+		.ui_clk_sync_rst(ui_clk_sync_rst),
+		.init_calib_complete(init_calib_complete),
+		.device_temp(device_temp),
+
+		.sys_clk_i(clk),
+		.clk_ref_i(clk),
+		.sys_rst(sys_rst)
+
+`ifdef SKIP_CALIB
+		,
+		.calib_tap_req(calib_tap_req),
+		.calib_tap_load(calib_tap_load),
+		.calib_tap_addr(calib_tap_addr),
+		.calib_tap_val(calib_tap_val),
+		.calib_tap_load_done(calib_tap_load_done)
+`endif
+	);
+
+	// 메모리 요청 트래픽에 따라 메모리를 read/write하는 state machine이 필요.
+
 
 endmodule
