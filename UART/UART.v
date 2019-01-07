52e2f221084271ec8c5d24f2f8aa3bc276551df2
lunar456th <lunar456th@naver.com>
2019-01-07 19:32:55 +0900

    add UART

--- UART/UART.v
+++ UART/UART.v
@@ -4,17 +4,25 @@
 // Clock freq : 100MHz 기준
 // Baud rate  : 115200 bps
 // Data bit   : 8 bits
-// Parity bit : no parity
+// Parity bit : None
 // stop bit   : 1 bit
 // 이것도 동적으로 작성은 가능할 듯.
 
 // UART Controller
+
+`define PARITY_NONE  0
+`define PARITY_ODD   1
+`define PARITY_EVEN  2
+`define PARITY_MARK  3
+`define PARITY_SPACE 4
+
 module UART_Controller # (
 	parameter CLK_FREQ = 100000000, // parameter 작업 마저 해야 함
 	parameter BAUD_RATE = 115200,
-	parameter DATA_BIT = 8,
-	parameter PARITY_BIT = 0,
-	parameter STOP_BIT = 1
+	parameter OVER_SAMPLES = 16,
+	parameter DATA_BITS = 8,
+	parameter PARITY = `PARITY_NONE,
+	parameter STOP_BITS = 1
 	)	(
 	input wire clk,
 	output wire tx,
@@ -26,15 +34,16 @@ module UART_Controller # (
 	reg read_clear;
 	reg read_complete;
 
-	reg [7:0] data_in;
-	reg [7:0] data_out;
+	reg [DATA_BITS-1:0] data_in;
+	reg [DATA_BITS-1:0] data_out;
 
 	UART # (
 		.CLK_FREQ(CLK_FREQ),
 		.BAUD_RATE(BAUD_RATE),
-		.DATA_BIT(DATA_BIT),
-		.PARITY_BIT(PARITY_BIT),
-		.STOP_BIT(STOP_BIT)
+		.OVER_SAMPLES(OVER_SAMPLES),
+		.DATA_BITS(DATA_BITS),
+		.PARITY(PARITY),
+		.STOP_BITS(STOP_BITS)
 	) _UART (
 		.clk(clk),
 		.tx(tx),
@@ -53,9 +62,8 @@ module UART_Controller # (
 		write_busy <= 1'b0;
 		read_clear <= 1'b0;
 		read_complete <= 1'b0;
-		data_in <= 8'b0;
-		data_out <= '8'b0;
-		state <= STATE_READY;
+		data_in <= 0;
+		data_out <= 0;
 	end
 
 	// 앵무새 테스트벤치
@@ -66,7 +74,6 @@ module UART_Controller # (
 			data_out <= data_in;
 			write_en <= 1'b1;
 			read_complete <= 1'b0;
-			state <= STATE_WRITE;
 		end
 	end
 
@@ -77,37 +84,40 @@ module UART # (
 	parameter CLK_FREQ = 100000000,
 	parameter BAUD_RATE = 115200,
 	parameter OVER_SAMPLES = 16,
-	parameter DATA_BIT = 8,
-	parameter PARITY_BIT = 0,
-	parameter STOP_BIT = 1
+	parameter DATA_BITS = 8,
+	parameter PARITY = `PARITY_NONE,
+	parameter STOP_BITS = 1
 	)	(
 	input wire clk,
 	output wire tx,
 	input wire rx,
-	input wire [7:0] data_in,
-	output wire [7:0] data_out,
+	input wire [DATA_BITS-1:0] data_in,
+	output wire [DATA_BITS-1:0] data_out,
 	input wire write_en,
 	output wire write_busy,
 	input wire read_clear,
 	output wire read_complete
 	);
 
-	wire rxclk_en;
-	wire txclk_en;
+	wire rx_clk_en;
+	wire tx_clk_en;
 
-	// Hacky baud rate generator to divide a 100MHz clock into a 115200 baud
+	// Generate baud rate by dividing a 100MHz clock into a 115200 baud.
 	// rx/tx pair where the rx clcken oversamples by 16x.
 	localparam RX_ACC_MAX = CLK_FREQ / (BAUD_RATE * OVER_SAMPLES); // 54.253
 	localparam TX_ACC_MAX = CLK_FREQ / BAUD_RATE; // 868.056
 	localparam RX_ACC_WIDTH = $clog2(RX_ACC_MAX); // 5.762 = 6
 	localparam TX_ACC_WIDTH = $clog2(TX_ACC_MAX); // 9.762 = 10
 
+	localparam DATA_BIT_WIDTH = $clog2(DATA_BITS);
+
 	reg [RX_ACC_WIDTH - 1:0] rx_acc = 0;
 	reg [TX_ACC_WIDTH - 1:0] tx_acc = 0;
 
-	assign rxclk_en = (rx_acc == 0);
-	assign txclk_en = (tx_acc == 0);
+	assign rx_clk_en = (rx_acc == 0);
+	assign tx_clk_en = (tx_acc == 0);
 
+	// generate Rx tick
 	always @ (posedge clk)
 	begin
 		if (rx_acc == RX_ACC_MAX[RX_ACC_WIDTH - 1:0])
@@ -120,6 +130,7 @@ module UART # (
 		end
 	end
 
+	// generate Tx tick
 	always @ (posedge clk)
 	begin
 		if (tx_acc == TX_ACC_MAX[TX_ACC_WIDTH - 1:0])
@@ -138,82 +149,118 @@ module UART # (
 		 tx <= 1'b1;
 	end
 
-	parameter STATE_READY = 2'b00;
-	parameter STATE_START = 2'b01;
-	parameter STATE_DATA = 2'b10;
-	parameter STATE_STOP = 2'b11;
+	localparam TX_STATE_READY = 2'b00;
+	localparam TX_STATE_START = 2'b01;
+	localparam TX_STATE_DATA = 2'b10;
+	localparam TX_STATE_STOP = 2'b11;
 
-	reg [7:0] data = 8'h00;
-	reg [2:0] index = 3'h0;
-	reg [1:0] state = STATE_READY;
+	reg [DATA_BITS-1:0] data = 0;
+	reg [DATA_BIT_WIDTH-1:0] index_tx = 0;
+	reg [1:0] state_tx = TX_STATE_READY;
 
 	always @ (posedge clk)
 	begin
-		case (state)
-			STATE_READY:
+		case (state_tx)
+			TX_STATE_READY:
 			begin
 				if (write_en)
 				begin
-					state <= STATE_START;
+					state_tx <= TX_STATE_START;
 					data <= data_in;
-					index <= 3'h0;
+					index_tx <= 0;
 				end
 			end
 
-			STATE_START:
+			TX_STATE_START:
 			begin
-				if (txclk_en)
+				if (tx_clk_en)
 				begin
-					tx <= 1'b0;
-					state <= STATE_DATA;
+					tx <= 1'b0; // start bit
+					state_tx <= TX_STATE_DATA;
 				end
 			end
 
-			STATE_DATA:
+			TX_STATE_DATA:
 			begin
-				if (txclk_en)
+				if (tx_clk_en)
 				begin
-					if (index == 3'h7)
-						state <= STATE_STOP;
+					if (index_tx == DATA_BIT_WIDTH - 1)
+					begin
+						state_tx <= TX_STATE_STOP;
+					end
 					else
-						index <= index + 3'h1;
-					tx <= data[index];
+					begin
+						index_tx <= index_tx + 1;
+					end
+					tx <= data[index_tx];
 				end
 			end
 
-			STATE_STOP:
+			TX_STATE_PARITY:
 			begin
-				if (txclk_en)
+				if (tx_clk_en)
 				begin
-					tx <= 1'b1;
-					state <= STATE_READY;
+					case (PARITY)
+						`PARITY_NONE:;
+						`PARITY_ODD:
+						begin
+							tx <= ~^data;
+						end
+
+						`PARITY_EVEN:
+						begin
+							tx <= ^data;
+						end
+
+						`PARITY_MARK:
+						begin
+							tx <= 1'b1; // mark bit = logic 1
+						end
+
+						`PARITY_SPACE:
+						begin
+							tx <= 1'b0; // space bit = logic 0
+						end
+						default:;
+					endcase
+					state_tx <= TX_STATE_STOP;
+				end
+			end
+
+			TX_STATE_STOP:
+			begin
+				if (tx_clk_en)
+				begin
+					tx <= 1'b1; // stop bit
+					state_tx <= TX_STATE_READY;
 				end
 			end
 
 			default:
 			begin
 				tx <= 1'b1;
-				state <= STATE_READY;
+				state_tx <= TX_STATE_READY;
 			end
 		endcase
 	end
 
-	assign write_busy = (state != STATE_READY);
+	assign write_busy = (state_tx != TX_STATE_READY);
 
 	// Rx
-	initial begin
+	initial
+	begin
 		read_complete <= 1'b0;
-		data <= 8'b0;
+		data <= 0;
 	end
 
-	parameter RX_STATE_START = 2'b00;
-	parameter RX_STATE_DATA = 2'b01;
-	parameter RX_STATE_STOP = 2'b10;
+	localparam RX_STATE_START = 2'b00;
+	localparam RX_STATE_DATA = 2'b01;
+	localparam RX_STATE_STOP = 2'b10;
 
-	reg [1:0] state = RX_STATE_START;
-	reg [3:0] sample = 4'd0;
-	reg [3:0] index = 4'd0;
-	reg [7:0] scratch = 8'd0;
+	reg [OVER_SAMPLES-1:0] sample = 0;
+	reg [OVER_SAMPLES-1:0] index_rx = 0;
+	reg [2*OVER_SAMPLES-1:0] scratch = 0;
+	reg [1:0] state_rx = RX_STATE_START;
 
 	always @(posedge clk)
 	begin
@@ -222,38 +269,38 @@ module UART # (
 			read_complete <= 1'b0;
 		end
 
-		if (rxclk_en)
+		if (rx_clk_en)
 		begin
-			case (state)
+			case (state_rx)
 				RX_STATE_START:
 				begin
 					// Start counting from the first low sample,
 					// once we've sampled a full bit, start collecting data bits.
-					if (!rx || sample != 4'd0)
+					if (!rx || sample != 0)
 					begin
-						sample <= sample + 4'd1;
+						sample <= sample + 1;
 					end
 
-					if (sample == 15)
+					if (sample == OVER_SAMPLES - 1)
 					begin
-						state <= RX_STATE_DATA;
-						index <= 3'd0;
-						sample <= 4'd0;
-						scratch <= 8'd0;
+						state_rx <= RX_STATE_DATA;
+						index_rx <= 0;
+						sample <= 0;
+						scratch <= 0;
 					end
 				end
 
 				RX_STATE_DATA:
 				begin
-					sample <= sample + 4'b1;
-					if (sample == 4'h8)
+					sample <= sample + 1;
+					if (sample == OVER_SAMPLES / 2)
 					begin
-						scratch[index[2:0]] <= rx;
-						index <= index + 4'b1;
+						scratch[index_rx[2:0]] <= rx;
+						index_rx <= index_rx + 1;
 					end
-					if (index == 8 && sample == 8'd15)
+					if (index_rx == OVER_SAMPLES / 2 && sample == OVER_SAMPLES - 1)
 					begin
-						state <= RX_STATE_STOP;
+						state_rx <= RX_STATE_STOP;
 					end
 				end
 
@@ -262,22 +309,22 @@ module UART # (
 					// Our baud clock may not be running at exactly the same rate as the transmitter.
 					// If we thing that we're at least half way into the stop bit,
 					// allow transition into handling the next start bit.
-					if (sample == 8'd15 || (sample >= 4'd8 && !rx))
+					if (sample == OVER_SAMPLES - 1 || (sample >= OVER_SAMPLES / 2 && !rx))
 					begin
-						state <= RX_STATE_START;
+						state_rx <= RX_STATE_START;
 						data <= scratch;
 						read_complete <= 1'b1;
-						sample <= 4'd0;
+						sample <= 0;
 					end
 					else
 					begin
-						sample <= sample + 4'b1;
+						sample <= sample + 1;
 					end
 				end
 
 				default:
 				begin
-					state <= RX_STATE_START;
+					state_rx <= RX_STATE_START;
 				end
 			endcase
 		end
--- UART/__UART.v
+++ UART/__UART.v
@@ -9,13 +9,7 @@
 // 이것도 동적으로 작성은 가능할 듯.
 
 // UART Controller
-module UART_Controller # (
-	parameter CLK_FREQ = 100000000, // parameter 작업 마저 해야 함
-	parameter BAUD_RATE = 115200,
-	parameter DATA_BIT = 8,
-	parameter PARITY_BIT = 0,
-	parameter STOP_BIT = 1
-	)	(
+module example_top (
 	input wire clk,
 	output wire tx,
 	input wire rx
@@ -29,13 +23,7 @@ module UART_Controller # (
 	reg [7:0] data_in;
 	reg [7:0] data_out;
 
-	UART # (
-		.CLK_FREQ(CLK_FREQ),
-		.BAUD_RATE(BAUD_RATE),
-		.DATA_BIT(DATA_BIT),
-		.PARITY_BIT(PARITY_BIT),
-		.STOP_BIT(STOP_BIT)
-	) _UART (
+	UART _UART(
 		.clk(clk),
 		.tx(tx),
 		.rx(rx),
@@ -57,33 +45,20 @@ module UART_Controller # (
 		data_out <= '8'b0;
 	end
 
-	// 앵무새 테스트벤치
 	always @ (posedge clk)
 	begin
-		if (read_complete && !write_busy)
-		begin
-			data_out <= data_in;
-			write_en <= 1'b1;
-			read_complete <= 1'b0;
-		end
+		// Do something...
 	end
 
 endmodule
 
 // UART Interface
-module UART # (
-	parameter CLK_FREQ = 100000000,
-	parameter BAUD_RATE = 115200,
-	parameter OVER_SAMPLES = 16,
-	parameter DATA_BIT = 8,
-	parameter PARITY_BIT = 0,
-	parameter STOP_BIT = 1
-	)	(
-	input wire clk,
-	output wire tx,
-	input wire rx,
-	input wire [7:0] data_in,
-	output wire [7:0] data_out,
+module UART (
+	input wire clk, // System clock: 100MHz
+	output wire tx, // Tx line
+	input wire rx, // Rx line
+	input wire [7:0] data_in, // Data in from Rx
+	output wire [7:0] data_out, // Data out to Tx
 	input wire write_en,
 	output wire write_busy,
 	input wire read_clear,
@@ -95,10 +70,15 @@ module UART # (
 
 	// Hacky baud rate generator to divide a 100MHz clock into a 115200 baud
 	// rx/tx pair where the rx clcken oversamples by 16x.
-	localparam RX_ACC_MAX = CLK_FREQ / (BAUD_RATE * OVER_SAMPLES); // 54.253
-	localparam TX_ACC_MAX = CLK_FREQ / BAUD_RATE; // 868.056
-	localparam RX_ACC_WIDTH = $clog2(RX_ACC_MAX); // 5.762 = 6
-	localparam TX_ACC_WIDTH = $clog2(TX_ACC_MAX); // 9.762 = 10
+	// 이렇게 해도 합성이 되나?
+	integer clk_freq = 100000000;
+	integer baudrate = 115200;
+	integer oversamples = 16;
+
+	parameter RX_ACC_MAX = clk_freq / (baudrate * oversamples); // 54.253
+	parameter TX_ACC_MAX = clk_freq / baudrate; // 868.056
+	parameter RX_ACC_WIDTH = $clog2(RX_ACC_MAX); // 5.762 = 6
+	parameter TX_ACC_WIDTH = $clog2(TX_ACC_MAX); // 9.762 = 10
 
 	reg [RX_ACC_WIDTH - 1:0] rx_acc = 0;
 	reg [TX_ACC_WIDTH - 1:0] tx_acc = 0;
@@ -136,19 +116,19 @@ module UART # (
 		 tx <= 1'b1;
 	end
 
-	parameter STATE_READY = 2'b00;
-	parameter STATE_START = 2'b01;
-	parameter STATE_DATA = 2'b10;
-	parameter STATE_STOP = 2'b11;
+	parameter STATE_IDLE	= 2'b00;
+	parameter STATE_START	= 2'b01;
+	parameter STATE_DATA	= 2'b10;
+	parameter STATE_STOP	= 2'b11;
 
 	reg [7:0] data = 8'h00;
 	reg [2:0] index = 3'h0;
-	reg [1:0] state = STATE_READY;
+	reg [1:0] state = STATE_IDLE;
 
 	always @ (posedge clk)
 	begin
 		case (state)
-			STATE_READY:
+			STATE_IDLE:
 			begin
 				if (write_en)
 				begin
@@ -184,23 +164,23 @@ module UART # (
 				if (txclk_en)
 				begin
 					tx <= 1'b1;
-					state <= STATE_READY;
+					state <= STATE_IDLE;
 				end
 			end
 
 			default:
 			begin
 				tx <= 1'b1;
-				state <= STATE_READY;
+				state <= STATE_IDLE;
 			end
 		endcase
 	end
 
-	assign write_busy = (state != STATE_READY);
+	assign write_busy = (state != STATE_IDLE);
 
 	// Rx
 	initial begin
-		read_complete <= 1'b0;
+		read_complete <= 0;
 		data <= 8'b0;
 	end
 
@@ -209,15 +189,15 @@ module UART # (
 	parameter RX_STATE_STOP = 2'b10;
 
 	reg [1:0] state = RX_STATE_START;
-	reg [3:0] sample = 4'd0;
-	reg [3:0] index = 4'd0;
-	reg [7:0] scratch = 8'd0;
+	reg [3:0] sample = 0;
+	reg [3:0] index = 0;
+	reg [7:0] scratch = 8'b0;
 
 	always @(posedge clk)
 	begin
 		if (read_clear)
 		begin
-			read_complete <= 1'b0;
+			read_complete <= 0;
 		end
 
 		if (rxclk_en)
@@ -225,19 +205,21 @@ module UART # (
 			case (state)
 				RX_STATE_START:
 				begin
-					// Start counting from the first low sample,
-					// once we've sampled a full bit, start collecting data bits.
-					if (!rx || sample != 4'd0)
+					/*
+					* Start counting from the first low sample, once we've
+					* sampled a full bit, start collecting data bits.
+					*/
+					if (!rx || sample != 0)
 					begin
-						sample <= sample + 4'd1;
+						sample <= sample + 4'b1;
 					end
 
 					if (sample == 15)
 					begin
 						state <= RX_STATE_DATA;
-						index <= 3'd0;
-						sample <= 4'd0;
-						scratch <= 8'd0;
+						index <= 0;
+						sample <= 0;
+						scratch <= 0;
 					end
 				end
 
@@ -249,7 +231,7 @@ module UART # (
 						scratch[index[2:0]] <= rx;
 						index <= index + 4'b1;
 					end
-					if (index == 8 && sample == 8'd15)
+					if (index == 8 && sample == 15)
 					begin
 						state <= RX_STATE_STOP;
 					end
@@ -257,15 +239,17 @@ module UART # (
 
 				RX_STATE_STOP:
 				begin
-					// Our baud clock may not be running at exactly the same rate as the transmitter.
-					// If we thing that we're at least half way into the stop bit,
-					// allow transition into handling the next start bit.
-					if (sample == 8'd15 || (sample >= 4'd8 && !rx))
+					/*
+					 * Our baud clock may not be running at exactly the same rate as the transmitter.
+					 * If we thing that we're at least half way into the stop bit,
+					 * allow transition into handling the next start bit.
+					 */
+					if (sample == 15 || (sample >= 8 && !rx))
 					begin
 						state <= RX_STATE_START;
 						data <= scratch;
 						read_complete <= 1'b1;
-						sample <= 4'd0;
+						sample <= 0;
 					end
 					else
 					begin
